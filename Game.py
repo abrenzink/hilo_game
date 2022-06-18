999994632bc75cda014bcebb3f4c64536e987759
abrenzink <a.brenzink@gmail.com>
2022-06-18 16:30:42 -0300

    final

--- Game.py
+++ Game.py
@@ -3,16 +3,11 @@ from Player import Player
 
 class Game:
     def __init__(self):
-        self.cards = []
         self.previous = 0
         self.current = 0
         self.is_playing = True
         self.score = 0
         self.total_score = 0
-
-        for i in range(13):
-            card = Card();
-            self.cards.append(card);
 
     def get_inputs(self):
         draw_card = input("Draw another card? [y/n] ")
@@ -23,37 +18,49 @@ class Game:
             return
 
         player = Player()
-        player.guess = input("The next drawn card will be higher or lower? [h/l] ")
+        answer = input(f"Do you bet next drawn card will be higher than {self.previous}? [y/n] ")
+        player.guess = (answer == "y")
 
         card = Card()
         card.draw()
         self.current = card.value
-        if (self.current > self.previous) else 
 
-        self.score += card.points 
+        result = self.compair_cards()
+        self.score = 100 if result == player.guess else -75
+
         self.total_score += self.score
+
+    def do_outputs(self):
+    
+        print(f"Previous card: {self.previous}")
+        print(f"Drawn card: {self.current}")
+        print(f"Your total score is: {self.total_score}\n")
 
         self.previous = self.current
 
-    def do_outputs(self):
-   
-        if not self.is_playing:
-            return
+        self.is_playing == (self.total_score < 0)
 
-        print(f"Drawn card: {self.current}\n")
-        print(f"Previous card: {self.previous}")
-        print(f"Your score is: {self.total_score}\n")
-        self.is_playing == (self.score > 0)
+        if not self.is_playing:
+            self.play_game()
+
+    def compair_cards(self):
+        if self.current > self.previous:
+            return True
+        else:
+            return False
 
     def play_game(self):
 
-        if self.previous == 0:
-            card = Card()
-            card.draw()
-            self.previous = card.value
-
         while self.is_playing:
+
+            if self.previous == 0:
+                card = Card()
+                card.draw()
+                self.previous = card.value
+
             self.get_inputs()
             self.do_updates()
             self.do_outputs()
+
+        print(f"Sorry, the game is over. Final score: {self.total_score}\n")
 
